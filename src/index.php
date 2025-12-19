801de1cc056d9e7687c836542c8c82c5af013328
takuma19339 <takuma19339@gmail.com>
2025-12-19 14:48:52 +0900

    a

--- src/index.php
+++ src/index.php
@@ -75,8 +75,9 @@ foreach ($questions as $qKey => $question) {
       </div>
     </section>
     <!-- /.p-hero .p-quiz-hero -->
-
+    <div class="p-quiz-container l-container">
     <?php for ($i = 0; $i < count($questions); $i++) { ?>
+    <section class="p-quiz-box js-quiz" data-quiz="<?= $i ?>">
       <div class="p-quiz-box__question">
         <h2 class="p-quiz-box__question__title">
           <span class="p-quiz-box__label">Q<?= $i + 1 ?></span>
@@ -89,15 +90,27 @@ foreach ($questions as $qKey => $question) {
       <div class="p-quiz-box__answer">
         <span class="p-quiz-box__label p-quiz-box__label--accent">A</span>
         <ul class="p-quiz-box__answer__list">
-          <?php foreach ($questions[$i][$choices] as $key => $choice) { ?>
+          <?php foreach ($questions[$i]['choices'] as $key => $choice) { ?>
             <li class="p-quiz-box__answer__item">
               <button class="p-quiz-box__answer__button js-answer" data-answer="<?= $key ?>" data-correct="<?php echo $choices["valid"]; ?>">
                 <?= $choice["name"]; ?><i class="u-icon__arrow"></i>
               </button>
             </li>
-<?php }} ?>
-    
-        <div class="p-quiz-container l-container">
+          <?php } ?>
+        </ul>
+        <div class="p-quiz-box__answer__correct js-answerBox">
+          <p class="p-quiz-box__answer__correct__title js-answerTitle"></p>
+          <p class="p-quiz-box__answer__correct__content">
+            <span class="p-quiz-box__answer__correct__content__label">A</span>
+            <span class="js-answerText"></span>
+          </p>
+        </div> 
+      </div>
+    </section>
+      <?php } ?>
+    </div>
+
+            <div class="p-quiz-container l-container">
       <section class="p-quiz-box js-quiz" data-quiz="0">
         <div class="p-quiz-box__question">
           <h2 class="p-quiz-box__question__title">
